dbf23f63c2b86aa4234969147a8fef3cde2784ce
Victor André Gris Costa <victorandr98@gmail.com>
2019-06-17 12:24:13 -0300

    Estrutura inicial

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Activation, Flatten, Dropout
